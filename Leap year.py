cfc32479bfc9b062d1754b1679c877497b05241c
Youssef Gamaleldin <youssefjamaleldin15@gmail.com>
2023-09-04 12:41:10 +0300

    leap_year

---
+++ Leap year.py
@@ -0,0 +1,17 @@
+#Problem_Solving
+
+#Leap_year
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+year = int(input("Enter a Year to check if it is a leap year: "))
+print(is_leap(year))
